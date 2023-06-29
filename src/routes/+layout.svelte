782525235642563f8ac7908402e1358155907df0
Patric Vormstein <pvormstein@googlemail.com>
2023-06-29 14:53:20 +0200

    finish form layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,13 @@
 	import '@picocss/pico';
 </script>
 
-<slot />
+<main class="container">
+	<h1>Subscription Message Generator</h1>
+	<slot />
+</main>
+
+<style>
+	.container {
+		margin-top: 50px;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<form action="">
+	<div class="grid">
+		<label for="operation_name">
+			Operation Name
+			<input type="text" id="operation_name" name="operation_name" placeholder="MyOperation" />
+		</label>
+		<label for="message_id">
+			Message ID
+			<input type="text" id="message_id" name="message_id" placeholder="1" />
+		</label>
+	</div>
+	<div class="grid">
+		<label for="operation">
+			Operation <span class="required-input">*</span>
+			<textarea id="operation" name="operation" rows="10" />
+		</label>
+		<label for="variables">
+			Variables
+			<textarea id="variables" name="variables" rows="10" />
+		</label>
+	</div>
+	<button type="submit">Generate</button>
+</form>
+
+<style>
+	.required-input {
+		color: red;
+	}
+</style>
